83b1005a6765db70df9e76a791566a387b67d828
Andrey Popp <8mayday@gmail.com>
2014-09-16 00:03:17 +0600

    Allow substyles to be of type Style

--- example/Button.js
+++ example/Button.js
@@ -53,9 +53,9 @@ var style = ReactStyle({
   backgroundColor: '#E6E6E6',
   textDecoration: none,
   borderRadius: '3px',
-  onActive: activeStyle.style,
-  onHover: hoverStyle.style,
-  onFocus: focusStyle.style
+  onActive: activeStyle,
+  onHover: hoverStyle,
+  onFocus: focusStyle
 });
 
 var Button = React.createClass({
